f49fa824af233f7f8d57c030b54c4be64518cd9c
redenval <worstgamedevs@gmail.com>
2019-09-22 19:08:56 +0800

    commit

--- disaster_last/Player_1.gd
+++ disaster_last/Player_1.gd
@@ -1,12 +1,22 @@
 extends KinematicBody2D
 
 var gravity_force = 100
+var move_speed = 400
 var velocity = Vector2()
 
 func _ready():
 	$AnimationPlayer.play("LefttoRight")
 	
 func _physics_process(delta):
-	velocity = Input.is_action_pressed("move_left") - Input.is_action_pressed("move_right")
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= move_speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x += move_speed
+	
+	if velocity.x > 0.1 or velocity.x < 0.1:
+		$AnimationPlayer.play("LefttoRight")
+	else:
+		$AnimationPlayer.stop(true)
 	velocity.y += gravity_force
 	move_and_slide(velocity)
