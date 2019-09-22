e8dabd027e52533d379bcf828f7d5515b05c0cb7
redenval <worstgamedevs@gmail.com>
2019-09-22 19:05:20 +0800

    commit

--- disaster_last/Player_1.gd
+++ disaster_last/Player_1.gd
@@ -7,5 +7,6 @@ func _ready():
 	$AnimationPlayer.play("LefttoRight")
 	
 func _physics_process(delta):
+	velocity = Input.is_action_pressed("move_left") - Input.is_action_pressed("move_right")
 	velocity.y += gravity_force
 	move_and_slide(velocity)
